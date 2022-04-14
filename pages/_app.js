2c1ff26f134bb9fc4a610267288a7e5fe3e78ad9
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-04-14 15:41:05 +0800

    fix google analytics not working

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,17 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
         {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'G-ZSCEHQLM4G', 'auto');
-          ga('send', 'pageview');
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
       </Script>
       <Navbar />
