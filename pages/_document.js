6b579cd905d1abf8c578640b2064882d3bce4f1d
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-04-14 22:33:58 +0800

    add most used languages

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,18 @@ import Navbar from "../components/Navbar";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Navbar />
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { NextSeo } from "next-seo";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -24,21 +25,32 @@ export default function Home() {
       />
 
       <main className={`${styles.container} ${styles.main}`}>
-        <h1 className="text-4xl my-2">Wee Hong KOH</h1>
-        <h2 className="text-2xl">Software Engineer</h2>
-        <div className="mt-5">
-          <a className="text-indigo-500" href="https://github.com/WeeHong/">
-            GitHub
-          </a>
-          <a
-            className="text-indigo-500 px-5"
-            href="https://www.linkedin.com/in/weehongayden/"
-          >
-            LinkedIn
-          </a>
-          <a className="text-indigo-500" href="https://medium.weehong.me/">
-            Medium
-          </a>
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+          <div className="flex flex-col items-center">
+            <h1 className="text-4xl my-2">Wee Hong KOH</h1>
+            <h2 className="text-2xl">Software Engineer</h2>
+            <div className="mt-5">
+              <a className="text-indigo-500" href="https://github.com/WeeHong/">
+                GitHub
+              </a>
+              <a
+                className="text-indigo-500 px-5"
+                href="https://www.linkedin.com/in/weehongayden/"
+              >
+                LinkedIn
+              </a>
+              <a className="text-indigo-500" href="https://medium.weehong.me/">
+                Medium
+              </a>
+            </div>
+            <div className="relative w-full h-48 mt-5">
+              <Image
+                src="https://github-readme-stats.vercel.app/api/top-langs/?username=WeeHong&layout=compact"
+                alt="Most Used Programming Languages"
+                layout="fill"
+              />
+            </div>
+          </div>
         </div>
       </main>
     </div>
