1db569b05199ca033fe4eb2b7a394d41da6f9659
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-04-14 16:42:44 +0800

    fix move script from _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </div>
   );
