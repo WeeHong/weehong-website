1732148071521feecda1b934a5ac1ce4298a2df8
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-04-15 09:45:27 +0800

    fix google analytics not working

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
       <Script id="google-analytics" strategy="lazyOnload">
         {`
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
       
-        gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
       </Script>
       <Component {...pageProps} />
