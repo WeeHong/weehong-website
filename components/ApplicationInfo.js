09bcdf5829c260aa37b485732d4a344904c5badd
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-04-16 12:31:57 +0800

    add applicant info

---
+++ components/ApplicationInfo.js
@@ -0,0 +1,130 @@
+import { PaperClipIcon } from "@heroicons/react/solid";
+
+const user = {
+  name: "Whitney Francis",
+  email: "whitney@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+};
+
+const attachments = [
+  {
+    name: "weehong-resume-2020-04-16.pdf",
+    href: "https://www.dropbox.com/s/nj87846k65l4f9t/WeeHong%20Resume%202022-04-16.pdf?dl=1",
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  return (
+    <div className="max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2">
+      <div className="space-y-6 lg:col-start-1 lg:col-span-2">
+        {/* Description list*/}
+        <section aria-labelledby="applicant-information-title">
+          <div className="bg-white/50 shadow sm:rounded-lg">
+            <div className="px-4 py-5 sm:px-6">
+              <h2
+                id="applicant-information-title"
+                className="text-lg leading-6 font-medium text-gray-900"
+              >
+                Applicant Information
+              </h2>
+              <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                Personal details and application.
+              </p>
+            </div>
+            <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+              <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                <div className="sm:col-span-1">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Application for
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-900">
+                    Backend Developer
+                  </dd>
+                </div>
+                <div className="sm:col-span-1">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Email address
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-900">
+                    <a
+                      className="text-accent"
+                      href="mailto:weehongkane@gmail.com"
+                    >
+                      weehongkane@gmail.com
+                    </a>
+                  </dd>
+                </div>
+                <div className="sm:col-span-1">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Nationality
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-900">
+                    Malaysian / Singapore PR
+                  </dd>
+                </div>
+                <div className="sm:col-span-1">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Education
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-900">
+                    B.S. in Information Technology
+                  </dd>
+                </div>
+                <div className="sm:col-span-2">
+                  <dt className="text-sm font-medium text-gray-500">About</dt>
+                  <dd className="mt-1 text-sm text-gray-900">
+                    I am a software engineer with 5+ years of experience in web
+                    development and experienced in API development, testing and
+                    debugging code. I am also equipped with the knowledge of
+                    CI/CD and software architecture design skillsets.
+                  </dd>
+                </div>
+                <div className="sm:col-span-2">
+                  <dt className="text-sm font-medium text-gray-500">
+                    Attachments
+                  </dt>
+                  <dd className="mt-1 text-sm text-gray-900">
+                    <ul
+                      role="list"
+                      className="border border-gray-200 rounded-md divide-y divide-gray-200"
+                    >
+                      {attachments.map((attachment) => (
+                        <li
+                          key={attachment.name}
+                          className="pl-3 pr-4 py-3 flex items-center justify-between text-sm"
+                        >
+                          <div className="w-0 flex-1 flex items-center">
+                            <PaperClipIcon
+                              className="flex-shrink-0 h-5 w-5 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            <span className="ml-2 flex-1 w-0 truncate">
+                              {attachment.name}
+                            </span>
+                          </div>
+                          <div className="ml-4 flex-shrink-0">
+                            <a
+                              href={attachment.href}
+                              className="font-medium text-blue-600 hover:text-blue-500"
+                            >
+                              Download
+                            </a>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
+import ApplicationInfo from "../components/ApplicationInfo";
 import { renderLayout, useLanguages } from "../components/GitHubStats";
 import styles from "../styles/Home.module.css";
 
@@ -27,8 +28,8 @@ const Home = ({ languages }) => {
       />
 
       <main className={`${styles.container} ${styles.main}`}>
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
-          <div className="flex flex-col items-center">
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
+          <div className="flex flex-col items-center relative intro-bg">
             <h1 className="text-4xl font-ibm font-bold my-2">Wee Hong KOH</h1>
             <h2 className="text-2xl font-ibm">Software Engineer</h2>
             <div className="mt-5">
@@ -51,7 +52,7 @@ const Home = ({ languages }) => {
                 Medium
               </a>
             </div>
-            <div className="flex justify-center items-center border border-slate-300 px-8 py-5 rounded mt-5">
+            <div className="flex justify-center items-center px-8 py-5 rounded mt-5 bg-white/50 shadow">
               <div>
                 <h3 className="font-ibm mb-7">
                   Most Used Programming Languages
@@ -63,6 +64,9 @@ const Home = ({ languages }) => {
                 />
               </div>
             </div>
+          </div>
+          <div className="col-span-2">
+            <ApplicationInfo />
           </div>
         </div>
       </main>
