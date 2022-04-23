a6eb35efda69a8074c1de564f02d567700ebb5e6
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-04-23 10:58:17 +0800

    fix client side error

--- pages/blog.js
+++ pages/blog.js
@@ -15,9 +15,8 @@ const Blog = ({ content: blogs }) => {
       </Head>
 
       <NextSeo
-        url={`https://www.weehong.me/blogs/${blog.Slug}`}
+        url={`https://www.weehong.me/blogs`}
         type="blog"
-        image={blog.Image}
         title="Wee Hong KOH - Blog"
         description="Wee Hong enjoys to writing code while learning new knowledge and write it into an article."
         canonical="https://www.weehong.me/blog"
